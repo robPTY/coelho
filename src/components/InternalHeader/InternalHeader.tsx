14f959d3f60ce3d9aeb22e753e0371141422f8eb
robPTY <rcadown8@gmail.com>
2025-01-07 18:48:49 -0600

    Finished V1 of internal header

--- src/components/InternalHeader/InternalHeader.tsx
+++ src/components/InternalHeader/InternalHeader.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { BiSolidHomeAlt2 } from "react-icons/bi";
+import { PiCursorFill } from "react-icons/pi";
+import { TbZoomFilled } from "react-icons/tb";
+import { FaHighlighter } from "react-icons/fa";
+import { IoIosUndo } from "react-icons/io";
+import { IoIosRedo } from "react-icons/io";
+import { IoChatbox } from "react-icons/io5";
+import { FaShareAlt } from "react-icons/fa";
 import "./InternalHeader.css";
 
 interface internalHeaderProps {
@@ -9,17 +17,34 @@ const InternalHeader: React.FC<internalHeaderProps> = ({ title }) => {
   return (
     <header className="internalHeader">
       <div className="pdfTools">
-        <button className="homeButton"></button>
-        <button className="cursorButton"></button>
-        <button className="highlightButton"></button>
-        <button className="zoomButton"></button>
-        <button className="undoButton"></button>
-        <button className="redoButton"></button>
+        <button className="homeButton">
+          <BiSolidHomeAlt2 className="headerIcon" />
+        </button>
+        <button className="cursorButton">
+          <PiCursorFill className="headerIcon" />
+        </button>
+        <button className="highlightButton">
+          <FaHighlighter className="headerIcon" />
+        </button>
+        <button className="zoomButton">
+          <TbZoomFilled className="headerIcon" />
+        </button>
+        <button className="undoButton">
+          <IoIosUndo className="headerIcon" />
+        </button>
+        <button className="redoButton">
+          <IoIosRedo className="headerIcon" />
+        </button>
       </div>
       <div className="pdfInfoContent">{title}</div>
       <div className="pdfOperations">
-        <button className="chatButton"></button>
-        <button className="shareButton"></button>
+        <button className="chatButton">
+          <IoChatbox className="headerIcon" />
+        </button>
+        <button className="shareButton">
+          <FaShareAlt className="headerIcon" />
+          Share
+        </button>
       </div>
     </header>
   );
